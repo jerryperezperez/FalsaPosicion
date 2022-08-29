b9d85af73f1946d4908faf34a32719ed5e6f740c
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2022-08-29 11:20:41 -0500

    Posible versión estable
    Creación del método abstracto.
    Reducción de variables.
    Mejoramiento de la estructura.
    Funcionamiento correcto con ejemplo.

--- src/FalsaPosicion.java
+++ src/FalsaPosicion.java
@@ -1,71 +1,85 @@
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import javax.swing.JOptionPane;
 import java.util.*;
+//TODO Mejorar posiblemente la estructura
+//TODO Organizar variables
+//TODO Mejorar el uso de DecimalFormat
 public abstract class FalsaPosicion {
-//más cambios que deben servir
-    //TODO Agregar variables de xi, xiAux, fxi y es como parámetros del constructor
-    public FalsaPosicion(){
-        System.out.println("HOLA");
+    private final double approximationError;
+    private DecimalFormatSymbols simbolos;
+    private DecimalFormat df;
+    private final double[] intervals, fx; //TODO Verificar que fx no se esté creando como variable final
+    private ArrayList<double[]> data;
+    private double xi, xiAux, fxi, es;
+
+    {
+        simbolos = new DecimalFormatSymbols();
+        simbolos.setDecimalSeparator('.');
+        df = new DecimalFormat("####.000000000", simbolos);
+        fx = new double[2];
+        data = new ArrayList<>();
+        xiAux = 0;
+        es = 100;
+    }
+    public FalsaPosicion(double approximationError, double intervalA, double intervalB){
+        this.approximationError = approximationError;
+        intervals = new double[] {intervalA, intervalB};
     }
 
-    public abstract void metodo();
+    public abstract double defineFormula(double var);
 
-    public void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DecimalFormatSymbols simbolos = new DecimalFormatSymbols();
-        simbolos.setDecimalSeparator('.');
-        DecimalFormat df = new DecimalFormat("####.000000000", simbolos);
+    public double useFormula(double var) {
+        return defineFormula(var);
+    }
 
-        double xi;
-        double xiAux = 0;
-        double fxi;
-        int contador = 0;
-        int iteracion;
-        double es = 100;
+    private void calculateFX(){
+        for (int i = 0; i < intervals.length; i++) {
+            fx[i] = Double.parseDouble(df.format(this.useFormula(intervals[i])));
+        }
+    }
 
-        double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Escribe tu intervalo a"));
-        double b = Double.parseDouble(JOptionPane.showInputDialog(null, "Escribe tu intervalo b"));
-
-        double fa = (double) ((a * Math.log10(a)) - 10);
-        double fb = (double) ((b * Math.log10(b)) - 10);
-        fa = Double.parseDouble(df.format(fa));
-        fb = Double.parseDouble(df.format(fb));
-
-        while (es > .05) {
-            contador = contador + 1;
-
-            fa = (double) ((a * Math.log10(a)) - 10);
-            fb = (double) ((b * Math.log10(b)) - 10);
-            fa = Double.parseDouble(df.format(fa));
-            fb = Double.parseDouble(df.format(fb));
-
-            xi = ((a * fb) - (b * fa)) / (fb - fa);
-            xi = Double.parseDouble(df.format(xi));
-            if (contador >= 2) {
+    public void calculate(){
+        this.calculateFX();
+        for (int counter = 1; es > approximationError; counter++) {
+          this.calculateFX();
+            xi = Double.parseDouble(df.format(((intervals[0] * fx[1]) - (intervals[1] * fx[0])) / (fx[1] - fx[0])));
+            if (counter >= 2) {
                 es = ((xi - xiAux) / xi) * 100;
             }
-
-            fxi = (double) ((xi * Math.log10(xi)) - 10);
-            fxi = Double.parseDouble(df.format(fxi));
-            System.out.print(contador + "   ");
-            System.out.print(a + "   ");
-            System.out.print(b + "   ");
-            System.out.print(fa + "   ");
-            System.out.print(fb + "   ");
-            System.out.print(xi + "   ");
-            System.out.print(fxi + "   ");
-            System.out.print(es + "   ");
-            System.out.println();
-
-            if ((fxi > 0 & fa > 0) ||(fxi < 0 & fa < 0)) {
-                a = xi;
-            }
-            if ((fxi > 0 & fb > 0) || (fxi < 0 & fb < 0)) {
-                b = xi;
+            fxi = Double.parseDouble(df.format(useFormula(xi)));
+            data.add(new double[]{counter, intervals[0], intervals[1], fx[0], fx[1], xi, fxi, es});
+            for (int i = 0; i < intervals.length; i++) {
+                if ((fxi > 0 & fx[i] > 0) || (fxi < 0 & fx[i] < 0)) {
+                    intervals[i] = xi;
+                }
             }
             xiAux = xi;
         }
+    }
+
+    public double getXi() {
+        return xi;
+    }
+
+    public ArrayList<double[]> getData() {
+        return data;
+    }
+
+    public double getEs() {
+        return es;
+    }
+
+    @Override
+    public String toString() { //TODO Emplear printf para mejorar el formato de impresión de la tabla
+        StringBuilder sb = new StringBuilder();
+        System.out.println("TAMAÑO DE LA TABLA: " + data.size());
+        for (double[] row : data) {
+            for (double value : row) {
+                sb.append(value + "   ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Vector;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-    FalsaPosicion nuevo = new FalsaPosicion() {
-        @Override
-        public void metodo() {
-            //TODO Inserción de código de función
-        }
-    };
-
+        FalsaPosicion falsa = new FalsaPosicion(.05, 8, 11) {
+            @Override
+            public double defineFormula(double var) {
+                return (var * Math.log10(var)) - 10;
+            }
+        };
+        falsa.calculate();
+        System.out.println(falsa.toString());
     }
+
 }
